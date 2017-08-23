bfe29ead1957a10919ef91fc00dad39fa4350e77
fon_harry <fon.harry@gmail.com>
2017-08-23 18:16:11 +0300

    Added simple ListView for Items. Added .gitignore. Fixed templates settings.

--- items/urls.py
+++ items/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from items.views import ItemListView
+
 
 urlpatterns = [
-    # url(r'^$',  ),
+    url(r'^$', ItemListView.as_view()),
 
 ]
--- items/views.py
+++ items/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from items.models import Item
 
-# Create your views here.
+
+class ItemListView(ListView):
+    model = Item
+
