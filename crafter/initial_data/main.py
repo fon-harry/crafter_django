6d4470f35a1124c9f212b4582f778981c7e00f58
fon_harry <fon.harry@gmail.com>
2017-08-27 11:07:30 +0300

    Some fixes in items parsing.

--- crafter/initial_data/main.py
+++ crafter/initial_data/main.py
@@ -11,10 +11,20 @@ def add_items_from_file(file_path):
     xml_file = etree.ElementTree(file=file_path)
     xml_tag_list = xml_file.getroot()
     for xml_tag_item in xml_tag_list:
+        for key in xml_tag_item.attrib:
+            if key == 'id':
+                atr_id = xml_tag_item.attrib[key]
+            elif key == 'type':
+                atr_name = xml_tag_item.attrib[key]
+            elif key == 'name':
+                atr_type = xml_tag_item.attrib[key]
+            else:
+                print('Atribut : %s in tag : %s not parsed.' % (key, xml_tag_item.tag))
+
         new_item = Item(
-                id=xml_tag_item.get('id'),
-                name=xml_tag_item.get('name'),
-                type=xml_tag_item.get('type')
+                id=atr_id,
+                name=atr_name,
+                type=atr_type
         )
 
         new_item.save()
@@ -30,6 +40,8 @@ def add_items_from_file(file_path):
                 new_param.save()
 
             elif i.tag == 'for':
+                pass
+            elif i.tag == 'cond':
                 pass
             else:
                 print('Item: %s with id: %s have non parsed tag: %s' % (xml_tag_item.get('name'), xml_tag_item.get('id'),i.tag))
@@ -61,7 +73,7 @@ def main():
     Item.objects.all().delete()
     ItemParam.objects.all().delete()
     add_items('./data/items')
-    print('Items added: %i with %i params' % (Item.objects.count(),ItemParam.objects.count()))
+    print('Items added: %i with %i params' % (Item.objects.count(), ItemParam.objects.count()))
 
     # recipes
     # add_recipes('./data/recipes.xml')
